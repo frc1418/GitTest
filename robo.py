a3cddb14baa312dbd1c65fc196867e135d77f7b8
carter.fendley <carter.fendley@gmail.com>
2016-10-13 11:55:14 -0400

    Added base files

---
+++ robo.py
@@ -0,0 +1,19 @@
+class Robot():
+    def __init__(self):
+        self.bender()
+        self.hal()
+    
+    def bender(self):
+        print('Kill all humans!')
+        
+    def hal(self):
+        print('I\'m sorry, Dave. I\'m afraid I can\'t do that.')
+        
+    def c3po(self):
+        print('R2-D2, it is you, It Is You !')
+    
+    def robo_cop(self):
+        print('Dead or alive, you\'re coming with me!')
+        
+if __name__ == '__main__':
+    robo = Robot()
